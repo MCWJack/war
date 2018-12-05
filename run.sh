d3ac3b44781b29fd0bcd43b34b03728c7f371dd1
Jack Littrell <jlittrell@mcw.edu>
2018-12-05 15:04:15 -0600

    run.sh now runs the program once, gathers the hands used, and reruns those many times to make stats easy

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
-END=5000
+readarray -t RESULT < <(./war.py -d)
+a="${RESULT[0]}"
+b="${RESULT[1]}"
+c="${RESULT[2]}"
+d="./war.py -1 \"$a\" -2 \"$b\""
+
+echo $a
+echo $b
+echo $c
+
+END=100
 for ((i=1;i<=END;i++)); do
-#    echo $i
-    ./war.py 
+    eval $d
 done
 
